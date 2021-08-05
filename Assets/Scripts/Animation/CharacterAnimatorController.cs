7ed00759dcd6383ac6e8237ec8e30da15465480d
Maxim Podgorov <mapodgorov@gmail.com>
2021-08-05 17:30:33 +0700

    animation and collider logic change

---
+++ Assets/Scripts/Animation/CharacterAnimatorController.cs
@@ -0,0 +1,8 @@
+public static class CharacterAnimatorController
+{
+    public static class Params
+    {
+        public const string Flying = "Flying";
+        public const string Falling = "Falling";
+    }
+}
