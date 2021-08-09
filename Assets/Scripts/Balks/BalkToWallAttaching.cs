befeca08c8e1ed1ae237d2cb9eddfcf0f5f3e80b
Maxim Podgorov <mapodgorov@gmail.com>
2021-08-10 00:59:02 +0700

    ball rotate from wall helper

---
+++ Assets/Scripts/Balks/BalkToWallAttaching.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalkToWallAttaching : MonoBehaviour
+{
+    [SerializeField] private MonoBehaviour _wallBehavior;
+    private IWall _wall => (IWall)_wallBehavior;
+
+    private void OnValidate()
+    {
+        if (!(_wallBehavior is IWall) || !_wallBehavior)
+            Debug.LogError(name + " needs to implement " + nameof(IWall));
+    }
+
+    [ContextMenu("RotateFromWall")]
+    private void RotateFromWall()
+    {
+        Vector3 direction = -_wall.GetNormalVector(transform.position);
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        transform.rotation = lookRotation;
+    }
+}
