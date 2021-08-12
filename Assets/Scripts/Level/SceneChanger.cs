c9c3a42df19628a3a0f44048bd1a8e0fe21abe86
Maxim Podgorov <mapodgorov@gmail.com>
2021-08-12 22:21:03 +0700

    Start scene and scene changing logic

---
+++ Assets/Scripts/Level/SceneChanger.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    [SerializeField] private int _totalLevels;
+    [SerializeField] private int _minCircledLevel;
+    [SerializeField] private int _maxCircledLevel;
+
+    public void LoadLevel(int targetlevel)
+    {
+        LoadScene(targetlevel);
+    }
+
+    private void LoadScene(int number)
+    {
+        SceneManager.LoadScene(PickSceneNumber(number));
+    }
+
+    private int PickSceneNumber(int number)
+    {
+        if (number <= _totalLevels)
+            return number;
+        else
+        {
+            int circledNumber = _minCircledLevel;
+
+            for (int i = _minCircledLevel; i < number; i++)
+            {
+                if (++circledNumber > _maxCircledLevel)
+                    circledNumber = _minCircledLevel;
+            }
+            return circledNumber;
+        }
+    }
+}
---
+++ Assets/Scripts/Level/StartScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScene : MonoBehaviour
+{
+    [SerializeField] private Level _level;
+    [SerializeField] private SceneChanger _sceneChanger;
+
+    private void Start()
+    {
+        _sceneChanger.LoadLevel(_level.CurrentLevel);
+    }
+}
