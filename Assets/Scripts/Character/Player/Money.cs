6945f24ee0dd20137741d0b22643c8d04168a46c
Maxim Podgorov <mapodgorov@gmail.com>
2021-08-12 20:17:08 +0700

    import and set UI

---
+++ Assets/Scripts/Character/Player/Money.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Money : MonoBehaviour
+{
+    private const string SAVED_MONEY = "MoneySaveID";
+
+    public event UnityAction<int> MoneyChanged;
+    public event UnityAction<int, int> LevelIncomeReady;
+
+    private int _currentLevelMoney = 0;
+    private int _currentMultiplier = 1;
+
+    public int CurrentMoney
+    {
+        get { return PlayerPrefs.GetInt(SAVED_MONEY, 0); }
+        private set { PlayerPrefs.SetInt(SAVED_MONEY, value); }
+    }
+
+    public void PullEvent()
+    {
+        MoneyChanged?.Invoke(CurrentMoney);
+    }
+
+    public void AddLevelMoney(int value)
+    {
+        if (value < 0)
+            throw new ArgumentException();
+
+        ChangeBalance(value);
+
+        CountLevelMoney(value);
+    }
+
+    public void AddLevelBonus(int value)
+    {
+        if (value < 0)
+            throw new ArgumentException();
+
+        ChangeBalance(value);
+    }
+
+    public bool TryRemoveMoney(int value)
+    {
+        if (value < 0)
+            throw new ArgumentException();
+
+        if (CurrentMoney > value)
+        {
+            ChangeBalance(-value);
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public void SetLevelBonusMultiplier(int multiplier)
+    {
+        if (multiplier < 1)
+            throw new ArgumentException();
+
+        _currentMultiplier = multiplier;
+        LevelIncomeReady?.Invoke(_currentLevelMoney, _currentMultiplier);
+
+        _currentLevelMoney = 0;
+        _currentMultiplier = 1;
+    }
+
+    private void ChangeBalance(int value)
+    {
+        CurrentMoney += value;
+        PullEvent();
+    }
+
+    private void CountLevelMoney(int value)
+    {
+        _currentLevelMoney += value;
+    }
+}
---
+++ Assets/Scripts/UI/Helpers/SmoothedTextValue.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class SmoothedTextValue : MonoBehaviour
+{
+    [SerializeField] private float _duration;
+    [SerializeField] private int _minValueChanges;
+
+    private Coroutine _waterfallJob;
+
+    public void StartSmooth(TMP_Text valueField, int startValue, int targetValue, string additionalText = "")
+    {
+        if (_minValueChanges > Mathf.Abs(targetValue - startValue))
+            valueField.text = additionalText + targetValue.ToString();
+        else
+        {
+            if (_waterfallJob != null)
+                StopCoroutine(_waterfallJob);
+            _waterfallJob = StartCoroutine(SmoothValue(valueField, startValue, targetValue, additionalText));
+        }
+    }
+
+    private IEnumerator SmoothValue(TMP_Text valueField, int startValue, int targetValue, string additionalText = "")
+    {
+        float lerp = 0f;
+
+        while (lerp < _duration)
+        {
+            lerp += Time.deltaTime;
+            valueField.text = additionalText + ((int)Mathf.Lerp(startValue, targetValue, lerp / _duration)).ToString();
+            yield return new WaitForFixedUpdate();
+        }
+
+        valueField.text = additionalText + targetValue.ToString();
+        _waterfallJob = null;
+    }
+}
