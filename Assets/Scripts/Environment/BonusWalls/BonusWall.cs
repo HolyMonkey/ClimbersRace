26de83d1b704cc10d935ca11add0326a6b4eb1fa
Maxim Podgorov <mapodgorov@gmail.com>
2021-08-17 06:32:19 +0700

    fix commit

--- Assets/Scripts/Environment/BonusWalls/BonusWall.cs
+++ Assets/Scripts/Environment/BonusWalls/BonusWall.cs
@@ -8,14 +8,6 @@ public class BonusWall : MonoBehaviour
     [SerializeField] private Transform _targetPoint;
     [SerializeField] private float _playerMoveDuration = 2f;
 
-    public Vector3 TargetPoint => _targetPoint.position;
-=========
-    [SerializeField] private float _playerMoveDuration =2f;
-
-    public Vector3 TargetPoint =>_targetPoint.position;
-=========
-    [SerializeField] private float _playerMoveDuration =2f;
-
     public Vector3 TargetPoint =>_targetPoint.position;
     public float PlayerMoveDuration => _playerMoveDuration;
     public int BonusMultiplier => _multiplier;
