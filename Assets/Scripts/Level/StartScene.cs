524f1cdaeac8aae1f154f9edd1ab06d4da83088b
Maxim Podgorov <mapodgorov@gmail.com>
2021-08-24 02:31:48 +0700

    import amplitude sdk

--- Assets/Scripts/Level/StartScene.cs
+++ Assets/Scripts/Level/StartScene.cs
@@ -6,9 +6,11 @@ public class StartScene : MonoBehaviour
 {
     [SerializeField] private Level _level;
     [SerializeField] private SceneChanger _sceneChanger;
+    [SerializeField] private AmplitudeAnalytics _amplitudeAnalytics;
 
     private void Start()
     {
+        _amplitudeAnalytics.GameStart();
         _sceneChanger.LoadLevel(_level.CurrentLevel);
     }
 }
