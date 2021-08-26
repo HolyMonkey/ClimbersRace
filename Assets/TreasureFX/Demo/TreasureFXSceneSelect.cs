e804373681733b6f63ce13196a4a5a35f11f3f8b
Maxim Podgorov <mapodgorov@gmail.com>
2021-08-26 22:54:26 +0700

    import treasureFX

---
+++ Assets/TreasureFX/Demo/TreasureFXSceneSelect.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TreasureFXSceneSelect : MonoBehaviour
+{
+    public void LoadSceneDemo1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+    public void LoadSceneDemo2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+    public void LoadSceneDemo3()
+    {
+        SceneManager.LoadScene("Scene3");
+    }
+    public void LoadSceneDemo4()
+    {
+        SceneManager.LoadScene("Scene4");
+    }
+    public void LoadSceneDemo5()
+    {
+        SceneManager.LoadScene("Scene5");
+    }
+    public void LoadSceneDemo6()
+    {
+        SceneManager.LoadScene("Scene6");
+    }
+    public void LoadSceneDemo7()
+    {
+        SceneManager.LoadScene("Scene7");
+    }
+    public void LoadSceneDemo8()
+    {
+        SceneManager.LoadScene("Scene8");
+    }
+    public void Load3DSceneDemo1()
+    {
+        SceneManager.LoadScene("3DScene1");
+    }
+    public void Load3DSceneDemo2()
+    {
+        SceneManager.LoadScene("3DScene2");
+    }
+    public void Load3DSceneDemo3()
+    {
+        SceneManager.LoadScene("3DScene3");
+    }
+}
