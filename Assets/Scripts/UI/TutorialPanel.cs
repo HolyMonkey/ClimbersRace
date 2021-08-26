78e9e4a312f03c6e34a5759fee8cf68b9ed5fa64
Maxim Podgorov <mapodgorov@gmail.com>
2021-08-26 16:11:44 +0700

    fix starttext level 1

--- Assets/Scripts/UI/TutorialPanel.cs
+++ Assets/Scripts/UI/TutorialPanel.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class TutorialPanel : MonoBehaviour
 {
     [SerializeField] Level _level;
+    [SerializeField] GameObject _startText;
 
     private void Awake()
     {
         if (_level && _level.CurrentLevel != 1)
+        {
             gameObject.SetActive(false);
+            _startText.SetActive(true);
+        }
     }
 }
