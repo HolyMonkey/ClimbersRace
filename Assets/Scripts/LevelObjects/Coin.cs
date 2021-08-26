bd62be917045362914f2a375e5ffdc016b2d48df
Maxim Podgorov <mapodgorov@gmail.com>
2021-08-27 00:15:21 +0700

    coins, fx, small changes

--- Assets/Scripts/LevelObjects/Coin.cs
+++ Assets/Scripts/LevelObjects/Coin.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class Coin : MonoBehaviour
 {
     [SerializeField] private int _rewardAmount = 1;
+    [SerializeField] private ParticleSystem _collectParticle;
 
     public int RewardAmount => _rewardAmount;
 
     public void Collect()
     {
+        _collectParticle.transform.parent = null;
+        _collectParticle.Play();
+
         Destroy(gameObject);
     }
 }
